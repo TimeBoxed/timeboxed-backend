f27f95cbbb3c669f52793307893aecb1eba98bce
dahby <dstoll243@gmail.com>
2018-07-24 21:28:11 -0700

    rebuilt middleware

---
+++ src/lib/logger.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: `{new Date().toDateString().replace(/ /g, '-')}.log`, level: 'verbose' }),
+    new winston.transports.Console({ format: winston.format.simple(), level: 'info'}),
+  ],
+});
+
+logger.INFO = 'info';
+logger.ERROR = 'error';
+logger.VERBOSE = 'verbose';
+
+export default logger;
