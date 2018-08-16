0760c97956f694f68bf36b6b0b7a2d50a9ec0117
Carl Olson <colson10@gmail.com>
2018-08-15 23:35:05 -0700

    added prehook to preferences model to save to profile

--- src/model/preferences.js
+++ src/model/preferences.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import HttpError from 'http-errors';
+import Profile from './profile';
 
 const preferencesSchema = mongoose.Schema({
   phoneNumber: {
@@ -11,8 +13,8 @@ const preferencesSchema = mongoose.Schema({
     type: String,
   },
   agendaReceiveTime: {
-    type: Date,
-    default: 'Not yet specified',
+    type: String,
+    default: '07:30',
   },
   taskLengthDefault: {
     type: Number,
@@ -30,5 +32,19 @@ const preferencesSchema = mongoose.Schema({
     ref: 'profile',
   },
 });
+
+function savePreHook(done) {
+  return Profile.findById(this.profile)
+    .then((profileFound) => {
+      if (!profileFound) throw new HttpError(404, 'Profile not found');
+      if (!profileFound.preferences) profileFound.preferences = this._id;
+      else return done();
+      return profileFound.save();
+    })
+    .then(() => done())
+    .catch(done);
+}
+
+preferencesSchema.pre('save', savePreHook);
 
 export default mongoose.model('preferences', preferencesSchema);
