03536150b46f51287c23cf2bd2e323148d522084
dahby <dstoll243@gmail.com>
2018-09-21 15:34:13 -0700

    cleaning up comments

--- src/model/preferences.js
+++ src/model/preferences.js
@@ -3,7 +3,7 @@
 import mongoose from 'mongoose';
 import HttpError from 'http-errors';
 import Profile from './profile';
-// import logger from '../lib/logger';
+
 
 const preferencesSchema = mongoose.Schema({
   phoneNumber: {
@@ -46,17 +46,6 @@ function savePreHook(done) {
     .catch(done);
 }
 
-// function prefPostHook(done) {
-//   return Profile.findByIdAndRemove(this.profile)
-//     .then((response) => {
-//       if (!response) return new HttpError(404, 'Profile not found in post hook.');
-//       logger.log(logger.INFO, 'DELETE - Pdffddfdfsasdfreferences successfully deleted.');
-//       return response.sendStatus(204);
-//     })
-//     .catch(done);
-// }
-
 preferencesSchema.pre('save', savePreHook);
-// preferencesSchema.post('remove', prefPostHook);
 
 export default mongoose.model('preferences', preferencesSchema);
