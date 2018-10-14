9c4254a88915c613d2ed60d9906579766c53b826
dahby <dstoll243@gmail.com>
2018-10-14 11:45:35 -0700

    account delete button now deletes profile, tasks, and preferences

--- src/routes/account-router.js
+++ src/routes/account-router.js
@@ -4,6 +4,9 @@ import { Router } from 'express';
 import HttpError from 'http-errors';
 import logger from '../lib/logger';
 import Account from '../model/account';
+import Profile from '../model/profile';
+import Preferences from '../model/preferences';
+import Tasks from '../model/task';
 import bearerAuthMiddleware from '../lib/bearer-auth-middleware';
 
 const accountRouter = new Router();
@@ -12,6 +15,24 @@ accountRouter.delete('/account', bearerAuthMiddleware, (request, response, next)
   return Account.findById(request.account._id)
     .then((account) => {
       if (!account) return next(new HttpError(404, 'account not found'));
+      if (account.profile) {
+        Profile.findById(account.profile)
+          .then((profile) => {
+            if (profile.preferences) {
+              Preferences.findById(profile.preferences)
+                .then((pref) => {
+                  pref.remove();
+                });
+            }
+            while (profile.tasks.length > 0) {
+              Tasks.findById(profile.tasks.pop())
+                .then((task) => {
+                  task.remove();
+                });
+            }
+            profile.remove();
+          });
+      }
       logger.log(logger.INFO, 'DELETE - Account successfully deleted.');
       return account.remove();
     })
